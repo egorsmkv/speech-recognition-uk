16c31e92dbf8b22a822801b47c0a4b8323fee5e6
Yehor Smoliakov <yehors+gh@ukr.net>
2023-04-13 09:33:33 +0000

    Enhance the code

--- speech-to-text/test_nemo_1_17_lm.py
+++ speech-to-text/test_nemo_1_17_lm.py
@@ -5,16 +5,21 @@ pip install -U evaluate torch nemo-toolkit[all]
 import csv
 
 import torch
+import evaluate
 import nemo.collections.asr as nemo_asr
-from evaluate import load
 
 # Config
 model_name = 'theodotus/stt_uk_squeezeformer_ctc_ml'
 batch_size = 20
 device = 'cuda' # or cpu
+kenlm_path = 'lm.binary'
+lm_beam_size = 128
+lm_beam_alpha = 0.5
+lm_beam_beta = 1.5
+testset_file = './cv10_clean.csv'
 
 # Load the test dataset
-with open('./cv10_clean.csv') as f:
+with open(testset_file) as f:
     samples = list(csv.DictReader(f))
 
 # Load the model
@@ -26,18 +31,17 @@ asr_model.change_decoding_strategy(None)
 # Configure the pyctcdecode strategy
 decoding_cfg = asr_model.cfg.decoding
 decoding_cfg.strategy = 'pyctcdecode'
-decoding_cfg.beam.beam_size = 128
+decoding_cfg.beam.beam_size = lm_beam_size
 decoding_cfg.beam.return_best_hypothesis = True
-decoding_cfg.beam.beam_alpha = 0.5
-decoding_cfg.beam.beam_beta = 1.5
-decoding_cfg.beam.kenlm_path = 'lm.binary'
+decoding_cfg.beam.beam_alpha = lm_beam_alpha
+decoding_cfg.beam.beam_beta = lm_beam_beta
+decoding_cfg.beam.kenlm_path = kenlm_path
 
 decoding_cfg.beam.pyctcdecode_cfg.beam_prune_logp = -10.0
 decoding_cfg.beam.pyctcdecode_cfg.token_min_logp = -5.0
 
 # decoding_cfg.beam.pyctcdecode_cfg.hotwords = []
 # decoding_cfg.beam.pyctcdecode_cfg.hotword_weight = 10.0
-
 
 # Use the pyctcdecode strategy
 asr_model.change_decoding_strategy(decoding_cfg)
@@ -49,8 +53,8 @@ def make_batches(iterable, n=1):
         yield iterable[ndx:min(ndx + n, l)]
 
 # Load evaluators
-wer = load('wer')
-cer = load('cer')
+wer = evaluate.load('wer')
+cer = evaluate.load('cer')
 
 # Temporary variables
 predictions_all = []
@@ -77,10 +81,9 @@ for batch in make_batches(samples, batch_size):
     references_all.extend(references)
 
 # Evaluate
-final_wer_value = round(wer.compute(predictions=predictions_all, references=references_all), 4)
-final_cer_value = round(cer.compute(predictions=predictions_all, references=references_all), 4)
+wer_value = round(wer.compute(predictions=predictions_all, references=references_all), 4)
+cer_value = round(cer.compute(predictions=predictions_all, references=references_all), 4)
 
 # Print results
-print('---')
 print('Final:')
-print(f'WER: {final_wer_value} | CER: {final_cer_value}')
+print(f'WER: {wer_value} | CER: {cer_value}')
