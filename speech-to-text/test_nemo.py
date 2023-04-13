d9671505cebdf55ded16c34d2e9ee3ddc4627cea
Yehor Smoliakov <yehors+gh@ukr.net>
2023-04-13 09:56:41 +0000

    Update test_nemo.py

--- speech-to-text/test_nemo.py
+++ speech-to-text/test_nemo.py
@@ -1,49 +1,67 @@
+"""
+pip install -U evaluate torch nemo-toolkit[all]
+"""
+
+import csv
+
 import torch
+import evaluate
 import nemo.collections.asr as nemo_asr
-import numpy as np
-import os
-import scipy.io.wavfile as wavfile
 
-from tempfile import NamedTemporaryFile
-from datasets import load_metric, load_from_disk
+# Config
+model_name = 'theodotus/stt_uk_squeezeformer_ctc_ml'
+batch_size = 20
+device = 'cuda' # or cpu
+testset_file = './cv10_clean.csv'
 
-asr_model = nemo_asr.models.EncDecCTCModel.from_pretrained("theodotus/stt_uk_squeezeformer_ctc_ml", map_location=torch.device('cuda'))
+# Load the test dataset
+with open(testset_file) as f:
+    samples = list(csv.DictReader(f))
 
-cache_file_test = 'cv10_clean.data'
-ds = load_from_disk(cache_file_test)
+# Load the model
+asr_model = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name, map_location=torch.device(device))
 
+# Disable decoding strategy for the model
+asr_model.change_decoding_strategy(None)
 
-def map_to_pred(batch):
-    tmp_files = []
+# A util function to make batches
+def make_batches(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
 
-    for speech in batch["speech"]:
-        with NamedTemporaryFile(suffix='.wav', delete=False) as tmp:
-            wavfile.write(tmp.name, 16000, np.array(speech))
-            tmp_files.append(tmp.name)
+# Temporary variables
+predictions_all = []
+references_all = []
 
-    results = asr_model.transcribe(tmp_files)
+# Inference in the batched mode
+for batch in make_batches(samples, batch_size):
+    paths = [it['path'] for it in batch]
+    references = [it['text'] for it in batch]
+
+    # Transcribe the audio files
+    predictions = asr_model.transcribe(paths2audio_files=paths, batch_size=batch_size)
+
+    # Log outputs
+    print('---')
+    print('Predictions:')
+    print(predictions)
+    print('References:')
+    print(references)
+    print('---')
+
+    # Add predictions and references
+    predictions_all.extend(predictions)
+    references_all.extend(references)
+
+# Load evaluators
+wer = evaluate.load('wer')
+cer = evaluate.load('cer')
     
-    # results = results[0]
+# Evaluate
+wer_value = round(wer.compute(predictions=predictions_all, references=references_all), 4)
+cer_value = round(cer.compute(predictions=predictions_all, references=references_all), 4)
 
-    batch["predicted"] = [it.replace('’', "'") for it in results]
-    batch["target"] = [ it.strip() for it in batch["text"] ]
-
-    print(batch["predicted"])
-    print(batch["target"])
-
-    for tf in tmp_files:
-        os.unlink(tf)
-
-    return batch
-
-
-result = ds.map(map_to_pred, batched=True, batch_size=100, remove_columns=list(ds.features.keys()))
-
-wer = load_metric("wer.py")
-cer = load_metric("cer.py")
-
-predictions = [x.upper() for x in result["predicted"]]
-references = [x.upper() for x in result["target"]]
-
-print(f"WER: {round(wer.compute(predictions=predictions, references=references), 4)}")
-print(f"CER: {round(cer.compute(predictions=predictions, references=references), 4)}")
+# Print results
+print('Final:')
+print(f'WER: {wer_value} | CER: {cer_value}')
