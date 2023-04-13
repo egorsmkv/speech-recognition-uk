6959510bc0adfd53d6f1925c8a907786c77dc00e
Yehor Smoliakov <yehors+gh@ukr.net>
2023-04-13 15:49:21 +0000

    Update test_whisper_transformers.py

--- speech-to-text/test_whisper_transformers.py
+++ speech-to-text/test_whisper_transformers.py
@@ -1,79 +1,79 @@
+"""
+pip install -U evaluate torch soundfile transformers
+"""
+
+import csv
+
 import torch
-import librosa
-import numpy as np
-import scipy.io.wavfile as wavfile
-
-from tempfile import NamedTemporaryFile
+import evaluate
+import soundfile as sf
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
-from datasets import load_dataset
-from datasets import load_metric, load_from_disk
 
-print('libs loaded')
+# Config
+model_name = 'Yehor/whisper-small-ukrainian'
+batch_size = 20
+sampling_rate = 16_000
+device = 'cuda' # or cpu
+testset_file = './cv10_clean.csv'
 
-model = WhisperForConditionalGeneration.from_pretrained("arampacha/whisper-large-uk-2")
-model.to('cuda')
-processor = WhisperProcessor.from_pretrained("arampacha/whisper-large-uk-2")
+# Load the test dataset
+with open(testset_file) as f:
+    samples = list(csv.DictReader(f))
 
-print('models loaded')
+# Load the model
+asr_model = WhisperForConditionalGeneration.from_pretrained(model_name).to(device)
+processor = WhisperProcessor.from_pretrained(model_name)
 
-cache_file_test = 'cv10_clean.data'
-test_set = load_from_disk(cache_file_test)
+# A util function to make batches
+def make_batches(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
 
-print('dataset loaded')
+# Temporary variables
+predictions_all = []
+references_all = []
 
-wer = load_metric("wer.py")
-cer = load_metric("cer.py")
+# Inference in the batched mode
+for batch in make_batches(samples, batch_size):
+    paths = [it['path'] for it in batch]
+    references = [it['text'] for it in batch]
 
+    # Extract audio
+    audio_inputs = []
+    for path in paths:
+        audio_input, _ = sf.read(path)
+        audio_inputs.append(audio_input)
 
-def map_to_pred(batch):
-    # save speech to files
-    tmp_files = []
+    # Transcribe the audio
+    input_features = processor(audio_inputs, sampling_rate=sampling_rate, return_tensors='pt').input_features.to(device)
 
-    for speech in batch["speech"]:
-        with NamedTemporaryFile(suffix='.wav', delete=False) as tmp:
-            wavfile.write(tmp.name, 16_000, np.array(speech))
-            tmp_files.append(tmp.name)
+    with torch.no_grad():
+        generated_ids = asr_model.generate(inputs=input_features, max_length=448)
 
-    # load files
-    audios = []
-    for path in tmp_files:
-        audio, _ = librosa.load(path)
-        audios.append(audio)
+    transcription = processor.batch_decode(generated_ids, normalize=True, skip_special_tokens=True)
+    predictions = [processor.tokenizer._normalize(it) for it in transcription]
 
-    input_features = processor(audios, return_tensors="pt", sampling_rate=16_000).input_features
-    input_features = input_features.to('cuda')
-    try:
-        generated_ids = model.generate(inputs=input_features)
+    # Log outputs
+    print('---')
+    print('Predictions:')
+    print(predictions)
+    print('References:')
+    print(references)
+    print('---')
 
-        transcription = processor.batch_decode(generated_ids, normalize=True, skip_special_tokens=True)
+    # Add predictions and references
+    predictions_all.extend(predictions)
+    references_all.extend(references)
 
-        batch['predicted'] = [processor.tokenizer._normalize(it) for it in transcription]
-        # batch['text'] = transcription
+# Load evaluators
+wer = evaluate.load('wer')
+cer = evaluate.load('cer')
+    
+# Evaluate
+wer_value = round(wer.compute(predictions=predictions_all, references=references_all), 4)
+cer_value = round(cer.compute(predictions=predictions_all, references=references_all), 4)
 
-        print('Ground truth:')
-        print(batch["text"])
-        print('Predicted text:')
-        print(batch["predicted"])
-
-    except IndexError:
-        # just a trick to pass the issue
-        batch['text'] = ['-'] * len(batch['speech'])
-        batch["predicted"] = ['-'] * len(batch['speech'])
-
-    predictions = [x.upper() for x in batch["predicted"]]
-    references = [x.upper() for x in batch["text"]]
-
-    print(f"WER: {round(wer.compute(predictions=predictions, references=references), 4)}")
-    print(f"CER: {round(cer.compute(predictions=predictions, references=references), 4)}")
-
-    return batch
-
-result = test_set.map(map_to_pred, batched=True, batch_size=10)
-
-print('-----')
-
-predictions = [x.upper() for x in result["predicted"]]
-references = [x.upper() for x in result["text"]]
-
-print(f"WER: {round(wer.compute(predictions=predictions, references=references), 4)}")
-print(f"CER: {round(cer.compute(predictions=predictions, references=references), 4)}")
+# Print results
+print('Final:')
+print(f'WER: {wer_value} | CER: {cer_value}')
